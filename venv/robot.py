381a3c96229c3fb998983159cc848b2249cf118e
JamesG3 <emislim12@gmail.com>
2018-04-09 23:17:25 -0400

    update

--- venv/robot.py
+++ venv/robot.py
@@ -5,11 +5,47 @@ class Robot(object):
 
 	def get_all_moves(self, board, player):
 		moves = []
-		pieces = []
+		pieces = board.check_jump(player)
 
+		if pieces == []:	# if there is no jump step for current player
+			for i in xrange(6):
+				for j in xrange(6):
+					if board.checkerBoard[i][j].piece\
+						and board.checkerBoard[i][j].piece.player == player:
+
+						valid_moves = board.valid_moves(i, j)
+						# if valid_moves:
+						for move in valid_moves:
+							moves.append([[i,j],move])
+		
+		else:				# if jump step exist
+			for piece in pieces:
+				valid_moves = board.valid_moves(piece[0], piece[1])
+				for move in valid_moves:
+					moves.append(piece, move)
+
+		
+		return moves 		# moves could be empty or only has one move
 
 
 	def make_move(self, board, player):
+		moves = self.get_all_moves(board, player)
+		if not moves:
+			return None
+
+		if len(moves) == 1:
+			return moves[0]
+
+		else:
+			return self.A_B_search(player)
+
+
+		# call get_all_moves() to get moves
+		# call A_B_search to get best move
+		# return that best move
+
+		# if there is no valid move, return None, and change turn in checkers.py
+		# else return piece and it's best move
 		return piece, move
 
 	def heuristic(self, ):
