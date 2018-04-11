2dfd15a07f45a4fdfcd8fbe78f962bae67a7044d
JamesG3 <emislim12@gmail.com>
2018-04-10 23:14:18 -0400

    update
    almost done

--- venv/robot.py
+++ venv/robot.py
@@ -7,7 +7,7 @@ class Robot(object):
 		pass
 
 
-	def get_all_moves(self, board, player):
+	def _get_all_moves(self, board, player):
 		moves = []
 		pieces = board.check_jump(player)
 
@@ -26,82 +26,62 @@ class Robot(object):
 			for piece in pieces:
 				valid_moves = board.valid_moves(piece[0], piece[1])
 				for move in valid_moves:
-					moves.append(piece, move)
+					moves.append([piece, move])
 
-		
 		return moves 		# moves could be empty or only has one move
 
 
-	def make_move(self, board, player):
-		moves = self.get_all_moves(board, player)
-		if not moves:
-			return None
-
-		if len(moves) == 1:
-			return moves[0]
-
-		else:
-			return self.A_B_search(player)
+	def _heuristic(self, board):
+		# if player == "white":
+			# return board.white_piece_Num
+		# else:
+			# return board.black_piece_Num
+		return board.white_piece_Num
 
 
-		# call get_all_moves() to get moves
-		# call A_B_search to get best move
-		# return that best move
-
-		# if there is no valid move, return None, and change turn in checkers.py
-		# else return piece and it's best move
-		return piece, move
-
-	def heuristic(self, board, player):
-		if player == "white":
-			return board.white_piece_Num
-		else:
-			return board.black_piece_Num
-		
-
-	
-
-	def A_B_search(self, board, moves):
+	def _A_B_search(self, board, moves):
 		# return final utility value and the curresponding next step 
-		v, action = self.max_value(baord, float('-Inf'), float('Inf'), conf.DEPTH)
+		v, action = self._max_value(baord, float('-Inf'), float('Inf'), conf.DEPTH)
 		return action
-		
 
-	def max_value(self, board, alpha, beta, depth):
+
+	def _max_value(self, board, alpha, beta, depth):
 		# if reach the depth, return utility value
 		if depth == 0:
-			return self.heuristic(board, player)
-		depth -= 1
-		moves = self.get_all_moves(board, player)
+			return self._heuristic(board), None
+		moves = self._get_all_moves(board, 'white')
 		v = float('-Inf')
 		action = None
 
 		for piece, move in moves:
-			tmp_board = board
 			# make move on tmp board
-			new_val = self.min_value(tmp_board, alpha, beta, depth-1)
+			tmp_board = self._make_move(board, piece, move)
+			new_val = self._min_value(tmp_board, alpha, beta, depth-1)
+			action = [piece, move]
 			if v < new_val:
 				v = new_val
-				action = [piece, move]
+			
 			if v >= beta:
-				return v
+				return v, action
 			alpha = max(alpha, v)
 
-		return v, action
+		if v == float('-Inf'):		# if there is no valid move -> terminal state
+			return self._heuristic(board), None
+		else:
+			return v, action
+			
 
-
-	def min_value(self, baord, alpha, beta, depth):
+	def _min_value(self, baord, alpha, beta, depth):
 		# if reach the depth, return utility value
-		if depth == 0 or not board.black_piece_Num or not board.white_piece_Num:
-			return self.heuristic(board, player)
-		depth -= 1
-		moves = self.get_all_moves(board, player)
+		if depth == 0:
+			return self._heuristic(board)
+		moves = self._get_all_moves(board, 'black')
 		v = float('Inf')
 
 		for piece, move in moves:
-			tmp_board = board
 			# make move on tmp_board
-			new_val = self.max_value(tmp_board, alpha, beta, depth-1)[0]
+			tmp_board = self._make_move(board, piece, move)
+			new_val = self._max_value(tmp_board, alpha, beta, depth-1)[0]
 			if v < new_val:
 				v = new_val
 
@@ -109,11 +89,42 @@ class Robot(object):
 				return v
 			beta = min(beta, v)
 
-		return v
+		if v == float('Inf'):		# if terminal state
+			return self._heuristic(board)
+		else:
+			return v
+
+
+	def _make_move(self, board, piece, move):
+		tmp_board = board
+		if abs(piece[0] - move[0]) == 2:		# jump step
+			tmp_board.remove([(piece[0] + move[0]) / 2, (piece[1] + move[1]) / 2])
+		tmp_board.move(piece, move)
+
+		return tmp_board
 
 
 		
+	def choose_move(self, board):
+		# return None if no move
+		# return the best (piece, move)
+		moves = self._get_all_moves(board, 'white')
+		if not moves:
+			return None
 
+		if len(moves) == 1:
+			return moves[0]
+
+		else:
+			return self._A_B_search(board)
+
+
+		# call _get_all_moves() to get moves
+		# call _A_B_search to get best move
+		# return that best move
+
+		# if there is no valid move, return None, and change turn in checkers.py
+		# else return piece and it's best move
 
 
 
